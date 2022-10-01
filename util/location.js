c3a5e4cdbc71490b9f29fe67245ba69bf7d71847
Sam Koh <sam910615@gmail.com>
2022-10-01 22:54:09 +0800

    update location file

--- util/location.js
+++ util/location.js
@@ -2,12 +2,13 @@ const axios = require('axios');
 
 const HttpError = require('../models/http-error');
 
-const API_KEY = process.GOOGLE_API_KEY;
+const API_KEY = process.env.GOOGLE_API_KEY;
 
 //async: to ensure the function waits for its respond only proceed
 async function getCoordsForAddress(address) {
     const response = await axios.get(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=`+ API_KEY
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=` + API_KEY
+        // `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=AIzaSyAN7y7BbckC5va5vQKPbvo-Am8e-GYEibo`
     );
 
     const data = response.data;
